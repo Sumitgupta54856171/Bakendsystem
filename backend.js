ce93b0a670a353531790bda3e79ff0ac436ecaba
Sumitgupta54856171 <sg54856171@gmail.com>
2025-08-07 08:56:24 +0000

    backend system

--- backend.js
+++ backend.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const { register, login } = require("./controller/sign");
 dotenv.config();
